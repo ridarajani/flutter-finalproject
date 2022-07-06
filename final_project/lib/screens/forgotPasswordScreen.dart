0354078292214a781d10607bbdc2486931ba905c
ridarajani <ridarajani125@gmail.com>
2022-07-06 12:50:18 -0700

    ui forgot password

--- final_project/lib/screens/forgotPasswordScreen.dart
+++ final_project/lib/screens/forgotPasswordScreen.dart
@@ -13,87 +13,108 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
-  final emailController = TextEditingController();
+  final emailaddressController = TextEditingController();
   @override
   void dispose() {
-    emailController.dispose();
+    emailaddressController.dispose();
     super.dispose();
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-            padding: const EdgeInsets.all(10),
-            child: ListView(
-              children: <Widget>[
-                Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(10),
-                    child: Image.asset('assets/images/registration.png')),
-                Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(10),
-                    child: const Text(
-                      'Forgot your Password?',
-                      style: TextStyle(
-                          color: Color.fromRGBO(239, 108, 98, 1),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30),
-                    )),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 0,
-                    vertical: 10,
-                  ),
-                  child: Column(
+      body: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/background.jpg'),
+                fit: BoxFit.cover),
+          ),
+          child: Padding(
+            padding: EdgeInsets.only(top: 250.0),
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 35, 10, 0),
+                child: ListView(children: [
+                  Column(
                     children: [
                       Container(
-                        padding: const EdgeInsets.all(10),
-                        child: TextFormField(
-                          controller: emailController,
-                          style: TextStyle(height: 0.5),
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            labelText: 'Email Address',
-                            labelStyle: TextStyle(
-                                color: Color.fromRGBO(165, 160, 160, 1)),
-                            fillColor: Color.fromRGBO(240, 240, 240, 0.86),
-                            filled: true,
-                            suffixIcon: Icon(Icons.email_outlined,
-                                color: Color.fromRGBO(165, 160, 160, 1)),
-                          ),
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          // validator: (email) => email != null && !EmailValidator.validate(email) ? 'Enter a valid email' : null,
+                          alignment: Alignment.center,
+                          // padding: const EdgeInsets.all(10),
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                                color: Color.fromRGBO(0, 67, 149, 1),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 0,
+                          vertical: 10,
                         ),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              child: TextFormField(
+                                controller: emailaddressController,
+                                style: TextStyle(height: 0.5),
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  labelText: 'Email Address',
+                                  labelStyle: TextStyle(
+                                      color: Color.fromRGBO(165, 160, 160, 1)),
+                                  fillColor:
+                                      Color.fromRGBO(240, 240, 240, 0.86),
+                                  filled: true,
+                                  suffixIcon: Icon(Icons.email_outlined,
+                                      color: Color.fromRGBO(165, 160, 160, 1)),
+                                ),
+                                autovalidateMode:
+                                    AutovalidateMode.onUserInteraction,
+                                // validator: (email) => email != null && !EmailValidator.validate(email) ? 'Enter a valid email' : null,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                        ),
+                        child: Container(
+                            height: 50,
+                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Color.fromRGBO(0, 67, 149, 1))),
+                              child: const Text('Reset Password',
+                                  style: TextStyle(
+                                      color:
+                                          Color.fromARGB(255, 252, 252, 252))),
+                              onPressed: () {
+                                //                 FirebaseAuth.instance
+                                // .sendPasswordResetEmail(email: emailaddressController.text.trim());
+                                print(emailaddressController.text.trim());
+                                verifyEmail();
+                              },
+                            )),
                       ),
                     ],
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                  ),
-                  child: Container(
-                      height: 50,
-                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                                Color.fromRGBO(129, 175, 161, 1))),
-                        child: const Text('Reset Password',
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 252, 252, 252))),
-                        onPressed: () {
-                            verifyEmail();
-                          // Navigator.push(
-                          //     context,
-                          //     MaterialPageRoute(
-                          //         builder: (_) => ResetPasswordScreen()));
-                        },
-                      )),
-                ),
-              ],
-            )));
+                ]),
+              ),
+            ),
+          )),
+    );
   }
 
   Future verifyEmail() async {
@@ -102,11 +123,13 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
         barrierDismissible: false,
         builder: (context) => Center(child: CircularProgressIndicator()));
     try {
+      print(emailaddressController.text.trim());
       await FirebaseAuth.instance
-          .sendPasswordResetEmail(email: emailController.text.trim());
+          .sendPasswordResetEmail(email: emailaddressController.text.trim());
       Navigator.of(context).popUntil((route) => route.isFirst);
     } on FirebaseAuthException catch (e) {
       print(e);
+      print('helooooo');
       Navigator.of(context).pop();
     }
   }
